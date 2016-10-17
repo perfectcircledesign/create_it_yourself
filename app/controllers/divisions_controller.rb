95957a4f6f25f506da3c81c2ac760755a2296444
Nicholas Erasmus <nerasmus.108@gmail.com>
2016-10-17 10:53:10 +0200

    redirect fix

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -13,9 +13,10 @@ class DivisionsController < ApplicationController
     if @company.divisions.count == 1
       division = @company.divisions.first
       redirect_to generate_division_path(division)
-    end
+    
     #I'M SORRY FOR THIS UGLY HACK :/
-    if @company.slug == "airschool43" and params[:function] != "crew_card"
+    elsif @company.slug == "airschool43" and params[:function] != "crew_card"
+
       redirect_to generate_division_path(@divisions.first)
     end
   
