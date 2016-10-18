bed8906b235d2a3c759f6a0d605c7be8baa0624e
Nicholas Erasmus <nerasmus.108@gmail.com>
2016-10-18 11:52:16 +0200

    fonts heroku

--- app/mailers/pdf_mailer.rb
+++ app/mailers/pdf_mailer.rb
@@ -11,7 +11,7 @@ class PdfMailer < ApplicationMailer
   def airschool43(pdf, division, card_holder_name, card_holder_email, purchase_order_number)
       
       attachments['crew_card.pdf'] = File.read(pdf) 
-      mail(:to => "gauragaura@gmail.com; simon@simonsays.co.za", :subject => "Online Crew Card Generator ", :body => "Good day\n\n43Airschool has requested business/crew cards for approval via the Online Card Generator, for #{card_holder_name}. The quantity and cost of the order corresponds to the Purchase Order Number (#{purchase_order_number}). From proof of receipt, the order will take 5 working days.\n\nThank you.\n\nRegards\n\nSimon Says Team")
+      mail(:to => "gauragaura@gmail.com", :subject => "Online Crew Card Generator ", :body => "Good day\n\n43Airschool has requested business/crew cards for approval via the Online Card Generator, for #{card_holder_name}. The quantity and cost of the order corresponds to the Purchase Order Number (#{purchase_order_number}). From proof of receipt, the order will take 5 working days.\n\nThank you.\n\nRegards\n\nSimon Says Team")
       # mail(:to => "info@simonsays.co.za; #{division.head_email_address}; #{card_holder_email}", :subject => "Online Business Card Generator ", :body => "Good day\n\n#{division.name} has requested business cards for approval via the Online Business Card Generator, for #{card_holder_name}. The quantity and cost of the order corresponds to the Purchase Order Number (#{purchase_order_number}). From proof of receipt, the order will take 5 working days.\n\nThank you.\n\nRegards\n\nCoca-Cola Team")
   end
 
