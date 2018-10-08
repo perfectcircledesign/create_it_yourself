b6e1719f5e67b184bab32441c04c599a09309679
Khanyiso <khanyiso@perfectcircle.co.za>
2018-10-08 17:38:00 +0200

    pdf margins

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -61,7 +61,7 @@ class DivisionsController < ApplicationController
                     margin:  { top: 0, bottom: 3, left: 5, right: 0 },
                       :align => 'right',
                         :save_to_file => Rails.root.join('tmp', "filename.pdf"),                                  
-                            :show_as_html => false, :dpi => '300', :save_only => true
+                            :show_as_html => true, :dpi => '300', :save_only => true
           else
             render :pdf => "filename.pdf",
                    :template => 'divisions/create_pdf.html.erb',#, :show_as_html => true
@@ -94,7 +94,7 @@ class DivisionsController < ApplicationController
         else
           
         end
-        redirect_to completed_job_division_path
+        # redirect_to completed_job_division_path
     end
     #END OF CARD FUNCTION
     if session[:function] == 'email_sig'
