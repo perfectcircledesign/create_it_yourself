c17848b2cebafdad33c55b33ebc73ed593e472d4
Khanyiso <khanyiso@perfectcircle.co.za>
2018-10-08 15:16:23 +0200

    pdf margins

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -57,7 +57,7 @@ class DivisionsController < ApplicationController
             :template => 'divisions/create_pdf.html.erb',#, :show_as_html => true
               :page_height => "#{@card_front.height / 300.0 *  25.4}",
                   :page_width => "#{@card_front.width / 300.0 * 25.4}",
-                      margin:  { top: 5, bottom: 3, left: 1, right: 1 },
+                      margin:  { top: 5, bottom: 3, left: 0, right: 1 },
                       :align => 'right',
                         :save_to_file => Rails.root.join('tmp', "filename.pdf"),                                  
                           :show_as_html => false, :dpi => '300', :save_only => true    
