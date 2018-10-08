69cc5f4dbe1f5c6f1f91047efaed4c4a74bbbd0b
Khanyiso <khanyiso@perfectcircle.co.za>
2018-10-08 16:45:41 +0200

    pdf margins

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -56,12 +56,12 @@ class DivisionsController < ApplicationController
           if @company.name.downcase == 'adendorff'
            render :pdf => "filename.pdf",
             :template => 'divisions/create_pdf.html.erb',#, :show_as_html => true
-              :page_height => "6.485cm",
-                  :page_width => "12.483cm",
+              :page_height => "6.485",
+                  :page_width => "12.483",
                     margin:  { top: 0, bottom: 3, left: 5, right: 0 },
                       :align => 'right',
                         :save_to_file => Rails.root.join('tmp', "filename.pdf"),                                  
-                            :show_as_html => true, :dpi => '300', :save_only => true
+                            :show_as_html => false, :dpi => '300', :save_only => true
           else
             render :pdf => "filename.pdf",
                    :template => 'divisions/create_pdf.html.erb',#, :show_as_html => true
