e133ad712433a7d0571aca52e8d169a1d4bcd5fc
Khanyiso <khanyiso@perfectcircle.co.za>
2018-10-09 08:46:56 +0200

    send pdf and calculate pdf size

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -56,8 +56,8 @@ class DivisionsController < ApplicationController
           if @company.name.downcase == 'adendorff'
            render :pdf => "filename.pdf",
             :template => 'divisions/create_pdf.html.erb',#, :show_as_html => true
-              :page_height => 6.485,
-                  :page_width => 12.483,
+                  :page_height => "#{@card_front.height / 300.0 *  25.4}",
+                  :page_width => "#{@card_front.width / 300.0 * 25.4}",
                     margin:  { top: 0, bottom: 3, left: 5, right: 0 },
                       :align => 'right',
                         :save_to_file => Rails.root.join('tmp', "filename.pdf"),                                  
@@ -94,7 +94,7 @@ class DivisionsController < ApplicationController
         else
           
         end
-        # redirect_to completed_job_division_path
+        redirect_to completed_job_division_path
     end
     #END OF CARD FUNCTION
     if session[:function] == 'email_sig'
