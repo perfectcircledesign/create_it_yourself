63d6704f4929fe00e95b1c735a53c2b53dcc70f5
Nick <nick@perfectcircle.co.za>
2019-11-14 12:58:28 +0200

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,12 +24,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
-  # Ensure that all assets are precompiled
-  config.assets.precompile += [ 'bootstrap.min.css', 'jquery.guillotine.css',
-  'application.css.scss','jquery.guillotine.js','application.js','bootstrap.min.js','cable.js','images.js', '*.js', '*.css']
-
+  
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
