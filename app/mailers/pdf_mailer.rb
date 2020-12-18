3fa1a94b4113b1b4f139ad86b168b00b6b8c651f
Nick <nick@perfectcircle.co.za>
2020-12-18 15:38:56 +0200

    add new email address for ardendorf

--- app/mailers/pdf_mailer.rb
+++ app/mailers/pdf_mailer.rb
@@ -14,16 +14,16 @@ class PdfMailer < ApplicationMailer
       
       attachments['card.pdf'] = File.read(pdf) 
       # mail(:to => "lindi@43airschool.com; attie@43airschool.com; vinca@perfectcircle.co.za", :subject => "Online Card Generator ", :body => "Good day\n\n43Airschool has requested business/crew cards for approval via the Online Card Generator, for #{card_holder_name}. Please find the card attached.\n\nThank you.\n\nRegards\n\nSimon Says Team")
-      mail(:to => "webadmin@perfectcircle.co.za, lauren@perfectcircle.co.za, calliandra@perfectcircle.co.za", :subject => "Online Card Generator ", :body => "Good day\n\n43Airschool has requested business/crew cards for approval via the Online Card Generator, for #{card_holder_name}. Please find the card attached.\n\nThank you.\n\nRegards\n\nSimon Says Team")
+      mail(:to => "gillian@adarchi.co.za", bcc:"webadmin@perfectcircle.co.za, lauren@perfectcircle.co.za, calliandra@perfectcircle.co.za", :subject => "Online Card Generator ", :body => "Good day\n\n43Airschool has requested business/crew cards for approval via the Online Card Generator, for #{card_holder_name}. Please find the card attached.\n\nThank you.\n\nRegards\n\nSimon Says Team")
   end
 
   def adendorff(pdf, division, card_holder_name, card_holder_email, purchase_order_number)
     puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#{card_holder_email}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     attachments['card.pdf'] = File.read(pdf)
     # mail(:to => "lauren@perfectcircle.co.za; vinca@perfectcircle.co.za; khanyiso@perfectcircle.co.za", :subject => "Online Card Generator ", :body => "Good day\n\nAdendorff has requested business/crew cards for approval via the Online Card Generator, for #{card_holder_name}. Please find the card attached.\n\nThank you.\n\nRegards\n\nAdendorff Team")
-    mail(:to => "webadmin@perfectcircle.co.za, lauren@perfectcircle.co.za, calliandra@perfectcircle.co.za", :subject => "Online Card Generator ", :body => "Good day\n\nAdendorff has requested business/crew cards for approval via the Online Card Generator, for #{card_holder_name}. Please find the card attached.\n\nThank you.\n\nRegards\n\nAdendorff Team")
-    # mail(:to => "nicholas@perfectcircle.co.za", :subject => "Online Business Card Generator ", :body => "Good day\n\n#{division.name} has requested business cards for approval via the Online Business Card Generator, for #{card_holder_name}. The quantity and cost of the order corresponds to the Purchase Order Number (#{purchase_order_number}). From proof of receipt, the order will take 5 working days.\n\nThank you.\n\nRegards\n\nCoca-Cola Team")
+    mail(:to => "gillian@adarchi.co.za", bcc:"webadmin@perfectcircle.co.za, lauren@perfectcircle.co.za, calliandra@perfectcircle.co.za", :subject => "Online Card Generator ", :body => "Good day\n\n#{card_holder_name} has requested business/crew cards for approval via the Online Card Generator, for #{card_holder_name}. Please find the card attached.\n\nThank you.\n\nRegards\n\nAdendorff Team")
   end
+
 
 
   # def email_simon_says(pdf, print_number )
